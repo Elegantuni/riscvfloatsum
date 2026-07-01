42504e48b420bc373553fb9543234a6ce428c472
Anthony Brown <groundup2360917182914017@gmail.com>
2026-07-01 07:25:09 -0400

    Refactor float addition and input/output handling
    Refactor main.s to simplify float addition and output.

--- main.s
+++ main.s
@@ -1,42 +1,30 @@
+
+# float float_sum(float a, float b)
+# a in fa0, b in fa1
+# Returns: sum in fa0
 .data
-output:
-	.ascii "The sum of %lf and %lf is %lf\n\0"
-input:
-	.ascii "Enter a number: \0"
-inputspec:
-	.ascii "%lf\0"
+output: .ascii: "The sum of %lf and %lf is %lf.\0"
+inputspec: .ascii "%lf\0"
+.float: number1, 4
+.float: number2, 4
 
 .text
 .globl main
 main:
-	addi sp, sp, -64
 
-	la a0, input
+float_sum:
+    # Add the single-precision floats in fa0 and fa1, store result in fa0
+
+	la fa0, number1
+	la fe1, number2
+	
+	fadd.s  fa0, fa0, fa1
+
+	la x0, output
+	la fa0, number1
+	la fe1, number2
 	call printf
-
-	la a0, inputspec
-	addi a1, sp, 8
-	call scanf
-
-	la a0, input
-	call printf
-
-	la a0, inputspec
-	addi a1, sp, 16
-	call scanf
-
-	fld fa0, 8(sp)
-	fld fa1, 16(sp)
-
-	fadd.d fa2, fa0, fa1
-
-	la a0, output
-	fmv.x.d a1, fa0
-	fmv.x.d a2, fa1
-	fmv.x.d a3, fa2
-	call printf
-
-	add sp, sp, 64
-
-	li a0, 0
-	call exit
+	
+    # Return to caller
+    ret
+	
